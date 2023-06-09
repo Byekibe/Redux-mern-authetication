2ef947c7fc7acf7dfbe0b20c125aa7110519844a
Byekibe <kibetpeter95@gmail.com>
2023-06-09 18:26:26 +0300

    Set up Mongo and Routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import colors from 'colors';
+import connectDB from './config/db.js'
 
 const app = express();
 const port = process.env.PORT || 5000;
+connectDB();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send("Server is ready");
 });
 
